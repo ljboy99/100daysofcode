2f077e1394d8e2c93d2247dacac76c6fdf47b81d
Louis Didomenicis <68966648+ljboy99@users.noreply.github.com>
2023-09-21 18:03:02 -0400

    Update blackjack.py
    fixed push

--- blackjack.py
+++ blackjack.py
@@ -27,9 +27,12 @@ A = 11
 cards = [A, 2, 3, 4, 5, 6, 7, 8, 9, 10, K, Q, J]
 # tens = ['10', 'King', 'Queen', 'Jack']
 
+
+
 playerhand = []
 dealerhand = []
 playerActive = None
+moneyfile = open("balance.txt", "r+")
 money = 100
 bet = 0
 gameresult = ''
@@ -57,6 +60,7 @@ def wager():
     bet = int(input(f"You have ${money}. How much are you betting?\n$"))
     money -= bet
     return bet
+
 
 def wagerwin():
     global money, bet
@@ -106,6 +110,7 @@ def dealerDrawCard():
         elif sum(dealerhand) == sum(playerhand) and sum(dealerhand) >= 17 and sum(playerhand) >= 17:
             print("PUSH!")
             gameresult = "TIE"
+            game()
             return gameresult
         elif A not in dealerhand and sum(dealerhand) > 21:
             print("The dealer busts! You win!")
@@ -178,7 +183,11 @@ def playerChoice():
         return playerActive, playerhand, bet
 
 def game():
-    wager()
+    global playerhand, dealerhand
+    playerhand = []
+    dealerhand = []
+    if gameresult != "TIE":
+      wager()
     playerFirstDraw()
     dealerFirstDraw()
     printPlayerHand()
